f0a56b22bb6d459ceace0387eda91465e2358cd9
Bharti <bharatifule10gmail.com>
2023-01-21 10:50:25 +0530

    Added:Line Comparision Problem

---
+++ bridgelabz/day/Main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day;
+
+public class Main {
+	 public static void main(String[] args) {
+
+		 LineComparision lineComapar = new LineComparision();
+		 LineComparision1 lineComparison = lineComapar.setCompareValues();
+		        lineComapar.geometryCalculation(lineComparison);
+		        lineComapar.compareOrEqual();
+
+		         }
+
+}
