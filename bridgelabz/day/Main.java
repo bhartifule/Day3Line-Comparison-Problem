3c7050e7e9b68b1e653da1a936833faccb5cb856
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-15 00:14:23 +0530

    Update Main.java

--- bridgelabz/day/Main.java
+++ bridgelabz/day/Main.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.day;
 
 public class Main {
-	 public static void main(String[] args) {
+	 public static void main(String[] args)  {
 
 		 LineComparision lineComapar = new LineComparision();
 		 LineComparision1 lineComparison = lineComapar.setCompareValues();
